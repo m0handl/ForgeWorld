a2add2cc070143bb78ed0a555a51f22a31e64989
m0handl <marcin.handl@outlook.com>
2021-10-13 09:46:59 +0200

    second

--- 01-storage.ps1
+++
@@ -1,7 +0,0 @@
-$rg = 'magazyn'
-#New-AzResourceGroup -Name $rg -Location westeurope -Force
-
-New-AzResourceGroupDeployment `
-    -Name 'magazyn43534535' `
-    -ResourceGroupName $rg `
-    -TemplateFile '01-storage.json'
